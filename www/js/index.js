a53ec2474c9b25492e66a8428ae8155f2d1a3e57
Alexandra Stoica <alexandra.stoica95@yahoo.com>
2015-11-09 11:24:57 +0000

    Added plugin

--- www/js/index.js
+++ www/js/index.js
@@ -5,14 +5,14 @@ function random(){
 		//beep once
 		navigator.notification.beep(1);
 		//vibrate for 2000 milliseconds
-		navigator.notification.vibrate(2000);
+		navigator.vibrate(2000);
 	} else{
 		//beep twice
 		navigator.notification.beep(2);
 		//wait for 4 seconds and then vibrate for 3 seconds
 		window.setTimeOut(
 			function(){
-			navigator.notification.vibrate(3000)
+				navigator.vibrate(3000);
 			},
 			4000
 		);
